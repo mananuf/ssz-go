fd02f3ee6451a779189a1055098460e1ed918178
mananuf <bmananaf@gmail.com>
2026-02-01 04:38:17 +0100

    feat: adds Batch container

--- pkg/types/containers.go
+++ pkg/types/containers.go
@@ -1,5 +1,10 @@
 package types
 
+type Batch struct {
+    Version uint32   // Fixed (4 bytes)
+    Data    []uint32 // Variable (requires an Offset!)
+}
+
 type Validator struct {
     ID             uint64
     EffectiveBalance uint64
