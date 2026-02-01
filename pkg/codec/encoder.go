6c6ceeb48cd762b0d9d516aa57cc183f47afa1e3
mananuf <bmananaf@gmail.com>
2026-02-01 05:05:29 +0100

    feat: adds logic for Batch Marshal implementation

--- pkg/codec/encoder.go
+++ pkg/codec/encoder.go
@@ -5,6 +5,25 @@ import (
 	"github.com/mananuf/ssz-go/pkg/types"
 )
 
+func MarshalBatch(b types.Batch) []byte {
+	// version = uint16/8 = 4bytes
+	// offset = 4 bytes
+	offsetSize := 8
+	totalByteSize := offsetSize + (len(b.Data) * 4)
+	buffer := make([]byte, totalByteSize)
+
+	copy(buffer[0:4], util.Uint32ToBytes(b.Version))
+	copy(buffer[4:8], util.Uint32ToBytes(uint32(offsetSize)))
+
+	startIndex := offsetSize
+	for _, data := range b.Data {
+		copy(buffer[startIndex:startIndex+4], util.Uint32ToBytes(uint32(data)))
+		startIndex += 4
+	}
+
+	return buffer
+}
+
 func MarshalValidator(v types.Validator) []byte {
 	buffer := make([]byte, 16)
 	copy(buffer[0:8], util.Uint64ToBytes(v.ID))
