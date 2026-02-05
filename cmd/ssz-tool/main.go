77b6753a9ee0f4ce6c0e1caf9c72153f8ced39a1
mananuf <bmananaf@gmail.com>
2026-02-05 03:21:38 +0100

    feat: adds merkleization implementation

--- cmd/ssz-tool/main.go
+++ cmd/ssz-tool/main.go
@@ -4,25 +4,53 @@ import (
 	"fmt"
 
 	"github.com/mananuf/ssz-go/pkg/codec"
+	"github.com/mananuf/ssz-go/pkg/merkle"
 	"github.com/mananuf/ssz-go/pkg/types"
 )
 
 func main() {
-	txs := types.Transaction {
+	txs := types.Transaction{
 		FromID: 10,
-		ToIDs: []uint16{10, 20, 30, 40},
+		ToIDs:  []uint16{10, 20, 30, 40},
 	}
 
-	txs2 := types.Transaction {
+	txs2 := types.Transaction{
 		FromID: 50,
-		ToIDs: []uint16{60, 70, 80, 90},
+		ToIDs:  []uint16{60, 70, 80, 90},
 	}
 
-	txBlock := types.TxBlock {
+	txBlock := types.TxBlock{
 		Txs: []types.Transaction{txs, txs2},
 	}
 
 	fmt.Println(txBlock)
 
 	codec.MarshalTxBatch(txBlock)
+
+	leaf0 := [32]byte{}
+	for i := range leaf0 {
+		leaf0[i] = 1
+	}
+
+	leaf1 := [32]byte{}
+	for i := range leaf1 {
+		leaf1[i] = 2
+	}
+
+	leaf2 := [32]byte{}
+	for i := range leaf2 {
+		leaf2[i] = 3
+	}
+
+	leaf3 := [32]byte{}
+	for i := range leaf3 {
+		leaf3[i] = 4
+	}
+
+	input := [][32]byte{leaf0, leaf1, leaf2, leaf3}
+
+	merkleRoot := merkle.HashTreeRoot(input)
+
+	fmt.Println(merkleRoot)
+
 }
--- pkg/merkle/tree.go
+++ pkg/merkle/tree.go
@@ -1,1 +1,43 @@
 package merkle
+
+import (
+	"crypto/sha256"
+)
+
+var ZeroCache [33][32]byte
+
+func Init() {
+	for i := 1; i < 33; i++ {
+		buffer := make([]byte, 64)
+		copy(buffer[0:32], ZeroCache[i-1][:])
+		copy(buffer[32:], ZeroCache[i-1][:])
+		ZeroCache[i] = sha256.Sum256(buffer)
+	}
+}
+
+func HashTreeRoot(leaves [][32]byte) [32]byte {
+	length := len(leaves)
+
+	if length == 0 {
+		return [32]byte{}
+	}
+
+	currentLayer := leaves
+
+	if length%2 != 0 {
+		currentLayer = append(currentLayer, ZeroCache[0])
+	}
+
+	nextLayer := make([][32]byte, 0, len(currentLayer)/2)
+
+	for i := 0; i < len(currentLayer); i += 2 {
+		buffer := make([]byte, 0, 64)
+		copy(buffer[0:32], currentLayer[i][:])
+		copy(buffer[32:64], currentLayer[i+1][:])
+		nextLayer = append(nextLayer, sha256.Sum256(buffer))
+	}
+
+	currentLayer = nextLayer
+
+	return currentLayer[0]
+}
